8c6029b2bd3245ce311867014bf788543a5132c8
weihanchen <willhanchen@gmail.com>
2016-07-29 15:06:07 +0800

    add user model

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'cribToken',
+  'database': 'mongodb://weihanchen:xxxxx@ds017432.mlab.com:17432/crib'
+}
---
+++ run.js
@@ -0,0 +1,29 @@
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var passport	= require('passport');
+var config      = require('./config/database'); // get db config file
+var User        = require('./app/models/user'); // get the mongoose model
+var port        = process.env.PORT || 8080;
+var jwt         = require('jwt-simple');
+ 
+// get our request parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+ 
+// log to console
+app.use(morgan('dev'));
+ 
+// Use the passport package in our application
+app.use(passport.initialize());
+ 
+// demo Route (GET http://localhost:8080)
+app.get('/', function(req, res) {
+  res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+ 
+// Start the server
+app.listen(port);
+console.log('There will be dragons: http://localhost:' + port);
