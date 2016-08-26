b644ad97be384eaaf211c347a6c291996c0d3112
weihanchen <willhanchen@gmail.com>
2016-08-26 13:48:14 +0800

    add travis.yml to continuous integration

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': process.env.SECRET_KEY || 'user_auth_demo',
-  'database': process.env.MONGO_CONNECTION || 'mongodb://user_auth_demo:a12345@ds031895.mlab.com:31895/user_auth_demo'
+  'database': process.env.MONGO_CONNECTION || 'mongodb://travis:test@127.0.0.1:27017/user_auth_demo'
 
 };
