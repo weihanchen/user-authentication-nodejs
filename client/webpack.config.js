38883694115444c73da10b3ec006f85ae8e6556e
weihanchen <willhanchen@gmail.com>
2016-12-09 17:19:41 +0800

    add index template

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,59 +1,73 @@
-var path = require('path');
-var webpack = require('webpack');
+let path = require('path');
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(__dirname, './client.js');
-var BUILD_PATH = path.resolve(__dirname, '../public');
+const ROOT_PATH = path.resolve(__dirname);
+const APP_PATH = path.resolve(__dirname, './client.js');
+const BUILD_PATH = path.resolve(__dirname, '../public');
+const INDEX_TEMPLATE_PATH = path.resolve(__dirname, './index.html');
+
 module.exports = {
-	entry: {
-		main: ['babel-polyfill', APP_PATH]
-	},
-	output: {
-		path: BUILD_PATH,
-		filename: 'bundle.min.js'
-	},
-	module: {
-		loaders: [{
-			test: /\.js[x]?$/,
-			exclude: /(node_modules|bower_components)/,
-			loaders: ['babel-loader?presets[]=es2015,presets[]=react']
-		}, {
-			test: /\.json$/,
-			loader: 'json'
-		}, {
-			test: /\.css$/,
-			loader: 'style-loader!css-loader'
-		}, {
-			test: /\.scss$/,
-			loaders: ["style", "css", "sass"]
-		}, {
-			test: /\.(png|jpg)$/,
-			loader: 'file-loader'
-		}, {
-			test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-			loader: "url-loader?limit=10000&mimetype=application/font-woff"
-		}, {
-			test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-			loader: "url-loader?limit=10000&mimetype=application/font-woff"
-		}, {
-			test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-			loader: "url-loader?limit=10000&mimetype=application/octet-stream"
-		}, {
-			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-			loader: "file"
-		}, {
-			test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-			loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-		}]
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			minimize: true
-		}),
-		new webpack.DefinePlugin({
-			"process.env": {
-				NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-			}
-		})
-	]
+    entry: {
+        main: ['babel-polyfill', APP_PATH]
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.min.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js[x]?$/,
+            exclude: /(node_modules|bower_components)/,
+            loaders: ['babel-loader?presets[]=es2015,presets[]=react']
+        }, {
+            test: /\.json$/,
+            loader: 'json'
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.scss$/,
+            loaders: ["style", "css", "sass"]
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'file-loader'
+        }, {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+        }, {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file"
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true
+        }),
+        new HtmlWebpackPlugin({
+            title: 'User Authentication',
+            template: INDEX_TEMPLATE_PATH,
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true
+            }
+        }),
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+            }
+        })
+    ]
 }
